3cff4bda67b93b02f3a5d7316180ec064f1551e3
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2022-03-18 21:04:01 +0100

    Working on registration

--- src/main/java/com/example/blog/Post/Post.java
+++ src/main/java/com/example/blog/Post/Post.java
@@ -1,12 +1,20 @@
 package com.example.blog.Post;
 
 import com.example.blog.User.ApplicationUser;
+import com.sun.istack.NotNull;
 
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "posts")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long PostID;
+
     @ManyToOne
+    @NotNull
     private ApplicationUser author;
 
 
